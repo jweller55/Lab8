60f2b872d745d8d41e572575cfa62365c2c4d6d4
Joel Weller <97563644+jweller55@users.noreply.github.com>
2023-03-09 19:17:50 -0700

    TestCount

--- app/src/main/java/com/example/lab8/CustomList.java
+++ app/src/main/java/com/example/lab8/CustomList.java
@@ -46,7 +46,7 @@ public class CustomList extends ArrayAdapter<City> {
     }
 
     public int getCount(){
-        return cities.size() + 1;
+        return cities.size();
     }
 
     public void addCity(City city){
