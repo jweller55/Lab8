f56cff04c15cf06161a8eab1e3b06aab2917f7f7
Joel Weller <97563644+jweller55@users.noreply.github.com>
2023-03-09 18:09:42 -0700

    hasCityTest

--- app/src/main/java/com/example/lab8/CustomList.java
+++ app/src/main/java/com/example/lab8/CustomList.java
@@ -52,5 +52,6 @@ public class CustomList extends ArrayAdapter<City> {
     public void addCity(City city){
         cities.add(city);
     }
+    public boolean hasCity(City city) {return false;}
 
 }
